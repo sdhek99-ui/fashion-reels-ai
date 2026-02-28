43c5a8055dd216116089981b12065510ce1bccf0
sdhek99-ui <sdhek99@gmail.com>
2026-02-28 07:02:48 +0700

    Create tailwind.config.js with dusty pink and sage green colors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'dusty-pink': '#d3a6a1',
+        'sage-green': '#b1cbb3',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
